13c5f97a0aff98de4e013f5e0c8cc159339bc49f
Oliver Ridsdale <oliver@natural-apptitude.co.uk>
2018-11-02 18:24:00 +0000

    Added basic reach-router

---
+++ src/Results.js
@@ -0,0 +1,95 @@
+import React from "react";
+import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+/*
+React class component
+# Class components can be more 'powerful' than function components.
+# Class components have access to component lifecycle methods for example.
+*/
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  // render() {
+  //   return React.createElement("div", {}, [
+  //     React.createElement(
+  //       "h1",
+  //       {
+  //         onClick: this.handleTitleClick
+  //       },
+  //       "Adopt Me!"
+  //     ),
+  //     React.createElement(Pet, {
+  //       name: "Luna",
+  //       animal: "dog",
+  //       breed: "havanese"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Pepper",
+  //       animal: "bird",
+  //       breed: "Cockatiel"
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Doink",
+  //       animal: "cat",
+  //       breed: "mixed"
+  //     })
+  //   ]);
+  // }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Seattle, WA" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({ pets });
+      });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(", ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Results;
--- src/app.js
+++ src/app.js
@@ -1,55 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import Results from "./Results";
+import Details from "./Details";
+import { Router } from "@reach/router";
 
-/*
-React class component
-# Class components can be more 'powerful' than function components.
-# Class components have access to component lifecycle methods for example.
-*/
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked the title");
-  }
-
-  // render() {
-  //   return React.createElement("div", {}, [
-  //     React.createElement(
-  //       "h1",
-  //       {
-  //         onClick: this.handleTitleClick
-  //       },
-  //       "Adopt Me!"
-  //     ),
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "dog",
-  //       breed: "havanese"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "bird",
-  //       breed: "Cockatiel"
-  //     }),
-  //     React.createElement(Pet, {
-  //       name: "Doink",
-  //       animal: "cat",
-  //       breed: "mixed"
-  //     })
-  //   ]);
-  // }
-
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Doin" animal="cat" breed="mixed" />
+        <h1>Adopt Me!</h1>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     );
   }
 }
 
-// render(React.createElement(App), document.getElementById("root"));
 render(<App />, document.getElementById("root"));
