5aff402cea268a34192e0ce1faff377921d3da48
Oliver Ridsdale <oliver.ridsdale@gmail.com>
2018-11-03 13:38:42 +0000

    Added adoption info fetching to details compoennt, added new carousel component

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,75 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+import Carousel from "./Carousel";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     loading: true
+  //   };
+  // }
+
+  /*
+  * To increae brevity we can use a custom babelrc configuration along with the 'transform-class-properties' plugin to set top level properties in our classes.
+  */
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({ output: "full", id: this.props.id })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city} , ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        navigate("/");
+      });
+  }
+
   render() {
-    return <h1>Hi lol</h1>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+    const { name, animal, breed, description, location, media } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
